6d5b0b180369f415d0216ad49dd385751cb5e18f
med-tlili <medaziztlilipro@gmail.com>
2025-02-16 15:45:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const wrapper=document.querySelector('.wrapper');
+const wrapperclient=document.querySelector('.wrapper-client');
+const loginLink=document.querySelector('.entreprise-link');
+const registerLink=document.querySelector('.register-link');
+const btnentreprise=document.querySelector('.btnentreprise-popup');
+const btnclient=document.querySelector('.btnclient-popup');
+const btnclientins=document.querySelector('.client-link');
+const btnentreprisecl=document.querySelector('.entreprise2-link');
+const icclose=document.querySelector('.icon-close');
+icclose.addEventListener('click',()=>{
+    wrapperclient.classList.remove('client');
+    wrapper.classList.remove('entreprise');
+});
+registerLink.addEventListener('click',()=>{
+    wrapper.classList.add('active');
+});
+loginLink.addEventListener('click',()=>{
+    wrapper.classList.remove('active');
+});
+btnentreprise.addEventListener('click',()=>{
+    wrapper.classList.add('entreprise');
+    wrapperclient.classList.remove('client');
+});
+btnclient.addEventListener('click',()=>{
+    wrapperclient.classList.add('client');
+    wrapper.classList.remove('entreprise');
+});
+btnentreprisecl.addEventListener('click',()=>{
+    wrapper.classList.add('entreprise');
+    wrapperclient.classList.remove('client');
+});
+btnclientins.addEventListener('click',()=>{
+    wrapperclient.classList.add('client');
+    wrapper.classList.remove('entreprise');
+});
